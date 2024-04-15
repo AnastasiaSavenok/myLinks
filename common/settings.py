265194c52cda4170dd5a2347309fb6fcde530210
AnastasiaSavenok <anastasiasavenok2828128@gmail.com>
2024-04-15 23:29:33 +0300

    Renamed project folder

--- common/settings.py
+++ common/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'xOneProject.urls'
+ROOT_URLCONF = 'common.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'xOneProject.wsgi.application'
+WSGI_APPLICATION = 'common.wsgi.application'
 
 
 # Database
